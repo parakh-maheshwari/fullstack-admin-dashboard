a044bc75fb11b97c62efb7bf12e0a85c27aeb94f
parakh-maheshwari <parakhmaheshwari03@gmail.com>
2025-12-20 11:14:04 +0530

    Use env variable for API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000"; 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL; 
 
 export async function getProjects() {
   const res = await fetch(`${BASE_URL}/dashboard/projects`);
